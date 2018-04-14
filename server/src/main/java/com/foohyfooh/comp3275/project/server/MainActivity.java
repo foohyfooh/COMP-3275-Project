858b0d37cbe347ce834d8e50ecdb4fed26612567
Darien117 <33788929+Darien117@users.noreply.github.com>
2018-04-14 18:04:34 -0300

    Some Changes and New Feature
    Fixed error in instructional words for server app, and added the Refresher pulldown functionality.

--- server/src/main/java/com/foohyfooh/comp3275/project/server/MainActivity.java
+++ server/src/main/java/com/foohyfooh/comp3275/project/server/MainActivity.java
@@ -1,6 +1,7 @@
 package com.foohyfooh.comp3275.project.server;
 
 import android.content.Intent;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +12,25 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private SwipeRefreshLayout swipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button gotoSendAlert = findViewById(R.id.main_gotoSendAlert);
+        swipe = (SwipeRefreshLayout)findViewById(R.id.swiperefresh);
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Intent intent3 = getIntent();
+                overridePendingTransition(0, 0);
+                intent3.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                finish();
+                overridePendingTransition(0, 0);
+                startActivity(intent3);
+            }
+        });
         ListView phoneNumbers = findViewById(R.id.main_phoneNumbers);
         final PhoneNumberManager phoneNumberManager = new PhoneNumberManager(this);
         final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, phoneNumberManager.getPhoneNumbers());
@@ -41,4 +55,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
 }
